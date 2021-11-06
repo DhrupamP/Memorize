1b471d9399b0dbfdf1f05ba47b236579d405bd9e
DhrupamP <pdhrupam@gmail.com>
2021-11-06 22:46:30 +0530

    minor change

--- lib/Comps/game_start/game_start.dart
+++ lib/Comps/game_start/game_start.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+void startTimer() {
+  const onesec = Duration(seconds: 1);
+}
 
 class GameStart extends StatefulWidget {
   const GameStart({Key? key}) : super(key: key);
@@ -20,6 +25,14 @@ class _GameStartState extends State<GameStart> {
               height: 200,
               width: 200,
               decoration: BoxDecoration(color: Colors.grey),
+            ),
+          ),
+          Align(
+            alignment: Alignment(0, 0.5),
+            child: Container(
+              height: 100,
+              width: 300,
+              color: Colors.grey,
             ),
           )
         ],
