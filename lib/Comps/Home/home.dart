84102166b8a19a11ebca367b12aa0672b67417ee
GLADI8R <danishmehmuda@gmail.com>
2021-11-06 20:54:11 +0530

    Update Kotlin Version

--- lib/Comps/Home/home.dart
+++ lib/Comps/Home/home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:memorizegame/Styles/constants.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,6 +9,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kBackGround,
       body: Center(
         child: Text('Hello World'),
       ),
