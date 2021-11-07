0c3c5cf545a6af1a5a5f76b58282efc7aa35bf47
GLADI8R <danishmehmuda@gmail.com>
2021-11-07 11:36:34 +0530

    Add Text Field

--- lib/Comps/game_end/game_end.dart
+++ lib/Comps/game_end/game_end.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -40,20 +42,44 @@ class _GameEndState extends State<GameEnd> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Color(0xff072540),
-      body: Align(
-        alignment: Alignment(0, -0.6),
-        child: Container(
-          height: 200,
-          width: 200,
-          decoration: BoxDecoration(color: Colors.grey),
-          child: Center(
-            child: Text(
-              _start.toString(),
-              style: const TextStyle(fontSize: 50, color: Colors.red),
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment(0, -0.6),
+            child: Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(color: Colors.grey),
+              child: Center(
+                child: Text(
+                  _start.toString(),
+                  style: const TextStyle(fontSize: 50, color: Colors.red),
+                ),
+              ),
             ),
           ),
-        ),
+          Align(
+            alignment: Alignment(0, 0.1),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 50, color: Colors.white),
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.pink),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.pink),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
